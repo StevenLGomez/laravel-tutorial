7b7e702aab132278913191695a3dbec47ce2f2d0
Steven Gomez <steve_gomez@usa.net>
2024-07-29 16:12:28 -0500

    Back in business, worked through to point of adding CSS file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html>
+<head>
+    <title>
+        @yield('title', 'Blog')
+    </title>
+    <link rel="stylesheet" href={{ asset('css/main.css') }}></link>
+</head>
+
+<body>
+
+<ul class="nav">
+    <li><a href="{{ route('home') }}">Home</a></li>
+    <li><a href="{{ route('about') }}">About</a></li>
+</ul>
+
+<div class="main">
+    @yield('content')
+</div>
+
+</body>
+</html>
+
+
