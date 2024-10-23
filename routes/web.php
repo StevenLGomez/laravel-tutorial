9cbd1d60609e0ba0f9d2871c53b5007fc7938967
Steven Gomez <steve_gomez@usa.net>
2024-10-23 13:01:48 -0500

    Refactoring, per video

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,25 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/posts/create', function () {
-    return view('create');
-})->name('posts.create');
+Route::name('posts.')->prefix('posts')->group(function () {
 
-Route::post('/posts', function (Request $request) {
-    // $request->input('title');
-    $request->validate([
-        'title' => 'required',
-        'description' => ['required', 'min:10'],
-        ]);
+    Route::get('/create', function () {
+        return view('posts.create');
+    })->name('create');
 
-    return redirect()
-        ->route('posts.create')
-        ->with('success', 'Post is submitted! Title: ' .
-            $request->input('title') . ' Description: ' .
-            $request->input('description'));
+    Route::post('/', function (Request $request) {
+        // $request->input('title');
+        $request->validate([
+            'title' => 'required',
+            'description' => ['required', 'min:10'],
+            ]);
 
-})->name('posts.store');
+        return redirect()
+            ->route('posts.create')
+            ->with('success', 'Post is submitted! Title: ' .
+                $request->input('title') . ' Description: ' .
+                $request->input('description'));
+    })->name('store');
+
+});
 
