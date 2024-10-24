96c1b71dbf8e739acba712fc2bb4d47abd68b324
Steven Gomez <steve_gomez@usa.net>
2024-10-23 21:01:16 -0500

    Finished Module 5

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,35 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [Homecontroller::class, 'home'])->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', [Homecontroller::class, 'about'])->name('about');
 
-Route::name('posts.')->prefix('posts')->group(function () {
+Route::resource('posts', PostController::class)->except(['index', ]);
 
-    Route::get('/create', function () {
-        return view('posts.create');
-    })->name('create');
+//Route::name('posts.')->prefix('posts')->group(function () {
 
-    Route::post('/', function (Request $request) {
-        // $request->input('title');
-        $request->validate([
-            'title' => 'required',
-            'description' => ['required', 'min:10'],
-            ]);
-
-        return redirect()
-            ->route('posts.create')
-            ->with('success', 'Post is submitted! Title: ' .
-                $request->input('title') . ' Description: ' .
-                $request->input('description'));
-    })->name('store');
-
-});
+//    Route::get('/create', function () {
+//        return view('posts.create');
+//    })->name('create');
+//
+//    Route::post('/', function (Request $request) {
+//        // $request->input('title');
+//        $request->validate([
+//            'title' => 'required',
+//            'description' => ['required', 'min:10'],
+//            ]);
+//
+//        return redirect()
+//            ->route('posts.create')
+//            ->with('success', 'Post is submitted! Title: ' .
+//                $request->input('title') . ' Description: ' .
+//                $request->input('description'));
+//    })->name('store');
+//
+//});
 
